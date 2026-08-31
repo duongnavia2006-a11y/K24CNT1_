562648359b7555b25f0a31609c14df8aaf7a65ad
duongnavia2006-a11y <duong.navia2006@gmail.com>
2026-08-31 12:48:05 +0700

    Delete

--- NtdLesson4Lab/NtdLesson4Lab/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NtdLesson4Lab.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
