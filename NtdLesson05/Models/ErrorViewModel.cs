42f7e2a679e36c2afe6cec1a7a732c2099414c64
Nguyen Tung Duong <duong.navia2006@gmail.com>
2026-09-06 15:37:00 +0700

    NtdLesson05Views

--- NtdLesson05/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NtdLesson05.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ NtdLesson05Views/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using NtdLesson05Views.Models;
+using System.Diagnostics;
+
+namespace NtdLesson05Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // lập trình trên view với Razor Code
+        public IActionResult RazorCode()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
